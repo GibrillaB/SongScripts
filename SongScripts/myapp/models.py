1f9681cb80de628ab1bde890647d89c32a2ad70d
deses1 <deses1@morgan.edu>
2024-12-01 21:25:19 -0500

    initial commit

--- SongScripts/myapp/models.py
+++ SongScripts/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Query(models.Model):
+    query_text = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"'{self.query_text}' at {self.timestamp}"
--- SongScripts/myapp/views.py
+++ SongScripts/myapp/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.conf import settings
 import requests
 from urllib.parse import urlencode
+from .models import Query
 
 def home(request):
     return render(request, "home.html")
 
+
 def search_lyrics(request):
     query = request.GET.get('query', '')  # Get the search term from user input
     search_results = None
+    
+
 
     if query:
+        #Save query to database
+        Query.objects.create(query_text=query)
         # API call to Genius to search for lyrics
         genius_url = f"https://api.genius.com/search?q={query}"
         headers = {'Authorization': f'Bearer {settings.GENIUS_ACCESS_TOKEN}'}
@@ -19,5 +25,7 @@ def search_lyrics(request):
         if response.status_code == 200:
             data = response.json()
             search_results = data.get('response', {}).get('hits', [])
-
-    return render(request, 'search.html', {'query': query, 'search_results': search_results})
+    #Retrieve the most recent queries
+    recent_queries = Query.objects.order_by('-timestamp')[:10]
+    
+    return render(request, 'search.html', {'query': query, 'search_results': search_results, 'recent_queries': recent_queries,})
